47091b91a0905210aa5014fe50a660fad08ef370
sya-ri <sya79lua@gmail.com>
2021-10-28 02:51:26 +0900

    chore: マイクラのバージョン毎にJarを生成するようにした close #6

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,40 +2,48 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 subprojects {
+    apply(plugin = "net.minecrell.plugin-yml.bukkit")
+
     dependencies {
         when (project.name) {
-            "common", "v1_8" -> {
-                implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+            "common" -> {
+                compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
             }
-        }
-        when (project.name) {
             "v1_8" -> {
-                api(project(":api:common"))
+                compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+                shadowApi(project(":api:common"))
+            }
+            "v1_13" -> {
+                compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+                shadowApi(project(":api:v1_8"))
             }
         }
     }
-}
 
-apply(plugin = "net.minecrell.plugin-yml.bukkit")
+    tasks.withType<ShadowJar> {
+        archiveBaseName.set("ktSpigot-${project.name}")
+    }
+
+    configure<BukkitPluginDescription> {
+        name = "ktSpigot"
+        version = "${rootProject.version}(${project.name})"
+        main = "dev.s7a.spigot.KtSpigot"
+        description = "Kotlin makes the Spigot library Easier"
+        author = "sya_ri"
+        website = "https://github.com/sya-ri/ktSpigot"
+        apiVersion = when (project.name) {
+            "v1_13" -> "1.13"
+            else -> null
+        }
+    }
+}
 
 dependencies {
     subprojects.forEach {
-        shadowApi(project(it.path)) {
-            exclude("org.spigotmc", "spigot-api")
-        }
+        shadowApi(project(it.path))
     }
 }
 
-tasks.withType<ShadowJar> {
-    archiveBaseName.set("ktSpigot")
-}
-
-configure<BukkitPluginDescription> {
-    name = "ktSpigot"
-    version = rootProject.version.toString()
-    main = "dev.s7a.spigot.KtSpigot"
-    description = "Kotlin makes the Spigot library Easier"
-    author = "sya_ri"
-    website = "https://github.com/sya-ri/ktSpigot"
-    apiVersion = "1.13"
+tasks.withType<Jar> {
+    enabled = false
 }
