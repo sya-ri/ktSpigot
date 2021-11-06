f624eb16c642326202a08ce505bd7bf752c149d8
sya-ri <sya79lua@gmail.com>
2021-11-06 13:23:49 +0900

    feat: メッセージ送信に関する拡張関数を追加

---
+++ api/v1_8/src/main/kotlin/dev/s7a/spigot/util/message.kt
@@ -0,0 +1,16 @@
+@file:JvmName("Message8")
+
+package dev.s7a.spigot.util
+
+import org.bukkit.command.CommandSender
+
+/**
+ * チャットに色付き文字列メッセージを送信する
+ *
+ * @param message メッセージ
+ * @param altColorChar セクションの代わりに使う文字 / '&'
+ * @since 1.0.0
+ */
+fun CommandSender.sendChatMessage(message: String, altColorChar: Char = '&') {
+    sendMessage(message.toColor(altColorChar))
+}
