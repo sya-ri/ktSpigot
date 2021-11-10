57a13c43fffdd2ed90447773acc150775e7813a9
sya-ri <sya79lua@gmail.com>
2021-11-10 17:24:43 +0900

    feat: itemStack を追加

--- api/v1_16/src/main/kotlin/dev/s7a/spigot/util/item.kt
+++ api/v1_16/src/main/kotlin/dev/s7a/spigot/util/item.kt
@@ -2,6 +2,8 @@
 
 package dev.s7a.spigot.util
 
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
 /**
@@ -11,3 +13,31 @@ import org.bukkit.inventory.meta.ItemMeta
  */
 val ItemMeta.customModelDataOrNull
     get() = if (hasCustomModelData()) customModelData else null
+
+/**
+ * [ItemStack] を生成する
+ *
+ * @param type マテリアル
+ * @param amount アイテム数 / 1
+ * @param displayName 表示名 / null
+ * @param lore 説明文 / null
+ * @param customModelData カスタムモデルデータ / null
+ * @param altColorChar [toColor] に使う文字 / '&'
+ * @param editMetaAction [ItemMeta] の変更処理 / null
+ * @return [ItemStack]
+ * @since 1.0.0
+ */
+fun itemStack(
+    type: Material,
+    amount: Int = 1,
+    displayName: String? = null,
+    lore: List<String>? = null,
+    customModelData: Int? = null,
+    altColorChar: Char? = '&',
+    editMetaAction: (ItemMeta.() -> Unit)? = null,
+): ItemStack {
+    return itemStack(type, amount, displayName, lore, altColorChar) {
+        setCustomModelData(customModelData)
+        editMetaAction?.invoke(this)
+    }
+}
