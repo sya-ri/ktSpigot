e80c19d8596540baadc3b1460f4964450c0f24ed
sya-ri <34268371+sya-ri@users.noreply.github.com>
2022-03-09 00:23:35 +0900

    chore: メインクラスのパッケージが間違っている問題を修正 (#154)

--- api/v1_8/src/main/kotlin/dev/s7a/ktspigot/config/extension.kt
+++ api/v1_8/src/main/kotlin/dev/s7a/ktspigot/config/extension.kt
@@ -4,6 +4,7 @@ package dev.s7a.ktspigot.config
 
 import dev.s7a.ktspigot.util.sendChatMessage
 import org.bukkit.command.CommandSender
+import org.bukkit.plugin.java.JavaPlugin
 
 /**
  * コンフィグエラーを出力する
@@ -16,4 +17,16 @@ fun List<KtConfigError>.printErrors(sender: CommandSender) {
     getErrors().forEach {
         sender.sendChatMessage("&e$it")
     }
+}
+
+/**
+ * リソースからコンフィグをコピーする
+ *
+ * @param plugin プラグイン
+ * @param resourceName リソース名
+ * @see KtConfigBase.onCreateNewFile
+ * @since 1.0.0
+ */
+fun KtConfig.copyFromResource(plugin: JavaPlugin, resourceName: String) {
+    plugin.getResource(resourceName).copyTo(file.outputStream())
 }
