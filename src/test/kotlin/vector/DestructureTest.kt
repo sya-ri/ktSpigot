4e588a5af3e0dabbf6a44110bcf7daa749422d9f
sya-ri <git@s7a.dev>
2022-03-14 18:40:06 +0900

    feat: Location#component5, 6 を追加

--- src/test/kotlin/vector/DestructureTest.kt
+++ src/test/kotlin/vector/DestructureTest.kt
@@ -3,6 +3,7 @@ package vector
 import dev.s7a.ktspigot.vector.component1
 import dev.s7a.ktspigot.vector.component2
 import dev.s7a.ktspigot.vector.component3
+import org.bukkit.util.Vector
 import randomVector
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -12,8 +13,6 @@ class DestructureTest {
     fun `vector destructure`() {
         val vector = randomVector()
         val (x, y, z) = vector
-        assertEquals(vector.x, x)
-        assertEquals(vector.y, y)
-        assertEquals(vector.z, z)
+        assertEquals(vector, Vector(x, y, z))
     }
 }
