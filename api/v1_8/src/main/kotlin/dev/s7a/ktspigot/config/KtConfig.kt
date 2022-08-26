ba0964dd6dad61bb1e2581eb7168277f9ed91b5f
sya-ri <git@s7a.dev>
2022-08-26 21:03:12 +0900

    style: 不要なインポートを削除

--- api/v1_8/src/main/kotlin/dev/s7a/ktspigot/config/KtConfig.kt
+++ api/v1_8/src/main/kotlin/dev/s7a/ktspigot/config/KtConfig.kt
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandSender
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
-import java.util.logging.Logger
 
 /**
  * コンフィグ
