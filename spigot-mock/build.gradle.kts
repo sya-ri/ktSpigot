97686a6d8d1dba543c6e71dd385bc8608db38d9b
sya-ri <git@s7a.dev>
2022-11-18 16:33:38 +0900

    Renew

--- spigot-mock/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    implementation(kotlin("test"))
-    implementation("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("reflect"))
-    testImplementation(project(":api:v1_19"))
-    testImplementation(project(":showcase"))
-}
--- spigot-mock/src/test/kotlin/component/util.kt
+++
@@ -1,9 +0,0 @@
-package component
-
-import net.md_5.bungee.api.chat.BaseComponent
-import net.md_5.bungee.chat.ComponentSerializer
-
-/**
- * [Array]<[BaseComponent]> を json の形式に変換する
- */
-fun Array<BaseComponent>.toJson(): String = ComponentSerializer.toString(this)
